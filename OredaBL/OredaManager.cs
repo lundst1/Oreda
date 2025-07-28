25c6f0566d1643c01efa4103a1bc6d82adab0f7e
lundst1 <rickard.lundstedt@gmail.com>
2025-07-28 22:42:25 +0200

    Added method to call Scan in FileSystemScanner

--- OredaBL/OredaManager.cs
+++ OredaBL/OredaManager.cs
@@ -1,13 +1,28 @@
-﻿namespace OredaBL
+﻿using OredaDL;
+
+
+namespace OredaBL
 {
     public class OredaManager
     {
+        //Private instance of class  FileSystemScanner.
+        private FileSystemScanner scanner = new FileSystemScanner();
+        //Private instance of class DirectoryItem.
+        private DirectoryItem scannedFiles;
         /// <summary>
         /// Constructor for the class OredaManager
         /// </summary>
         public OredaManager()
         {
-
+        }
+        /// <summary>
+        /// Method to scan file system.
+        /// Calls method scan of same name.
+        /// </summary>
+        /// <param name="path"></param>
+        public void Scan(string path) 
+        { 
+            scannedFiles = scanner.Scan(path);
         }
     }
 }
